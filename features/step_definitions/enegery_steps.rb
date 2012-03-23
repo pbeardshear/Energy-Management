fbf483135a2f1b31c12f46eb61718ee5f6085ef4
Doboy <doboy0@gmail.com>
2012-03-23 10:19:46 -0700

    added initial steps definitions without any body

---
+++ features/step_definitions/enegery_steps.rb
@@ -0,0 +1,8 @@
+Given /I am viewing "(.*)" hall/ do |hall|
+end
+
+When /I select "(.*)" from the time interval dropdown/ do |interval|
+end
+
+Then /I should see the usage graph for the past week/ do
+end
