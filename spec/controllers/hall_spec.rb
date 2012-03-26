58fb6433448929c97f20316f0dd5fdabf83ad6ea
pbeardshear <pbeardshear@berkeley.edu>
2012-03-25 21:52:42 -0700

    rearranged hall controller spec and added another test

--- spec/controllers/hall_spec.rb
+++ spec/controllers/hall_spec.rb
@@ -7,15 +7,22 @@ describe HallController do
         @hall_feature = Factory(:hallfeature, { :hall_id => @hall.id, :green_feature_id => @feature.id })
 	end
 	
-	describe 'it should see the graph for the specified hall'
-		get :details, :id=> @hall.id
-		response.should contain("?key=12345678")
+	describe 'access the graph data for the hall' do
+	  it 'should retrieve the graph from the hall' do
+	    @hall.stub(:get_graph)
+	    @hall.should_receive(:get_graph)
+	    get :details, :id => @hall.id
+	  end
+	  
+	  it 'should see the correct graph in the response' do
+	    get :details, :id=> @hall.id
+	    response.should contain("?key=12345678")
+	  end
 	end
 	
 	describe 'it should see the green features for the specified hall'
 		get :details, id=> @hall.id
 		response.should contain("This hall has flourscent light bulbs")
 	end
-	
-	
+    end
 end
