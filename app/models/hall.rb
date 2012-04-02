6c896361e2386aa4b9ef96c3027b0e50e2cf177f
saasbook <saasbook@saasbook.(none)>
2012-04-02 15:28:04 -0700

    added hall model

--- app/models/hall.rb
+++ app/models/hall.rb
@@ -3,4 +3,9 @@ class Hall < ActiveRecord::Base
   has_many :green_features, :through => :hall_features
   has_many :settings, :through => :pinned_halls
   belongs_to :operational_unit
+  
+  def get_graph (width = 700, height = 300, interval = 'week')
+    '<script src="http://my.pulseenergy.com/embed/?key=#{self.id}&width=#{width}&height=#{height}&interval=#{interval}"></script>'
+  end
 end
+
