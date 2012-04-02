14208a0d300686c709a041bed1b44eebd61b6f5b
Huan Do <doboy0@gmail.com>
2012-04-02 16:46:28 -0700

    made model spec work

--- spec/models/hall_spec.rb
+++ spec/models/hall_spec.rb
@@ -7,8 +7,9 @@ describe Hall do
 
   describe "retrieving the hall's energy data" do
     it 'should retrieve the properly formed embedded script' do
-      @hall.get_graph.should  match(/^<script src="http:\/\/my.pulseenergy.com\/embed\/.*<\/script>$/)
+      @hall.get_graph.should match(/^<script src='http:\/\/my.pulseenergy.com\/embed\/.*<\/script>$/)
     end
+
     it 'should return the proper key for this hall' do
       @hall.get_graph.should match(/\?key=12345678/)
     end
