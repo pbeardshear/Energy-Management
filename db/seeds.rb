7fc05fba70e60f98ed540bd66880100d38b93116
Huan Do <doboy0@gmail.com>
2012-04-02 19:31:00 -0700

    added a green feature, hall feature seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
-Hall.create(:name => "Shofix", :key =>"Bd5238Ki31GOL5ZewrQglA%3D%3D").save()
-
+if Hall.where(:id => 1).empty?
+  Hall.create(:id => 1,:name => "Shofix", :key =>"Bd5238Ki31GOL5ZewrQglA%3D%3D").save()
+  GreenFeature.create(:id =>1, :name => "Flouresnce", :content => "This hall has lights that flourse")
+  HallFeature.create(:hall_id => 1, :green_feature_id => 1)
+end
