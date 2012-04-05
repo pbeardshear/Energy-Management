b9b29868b2f559b0de0f1c4c5a42d4822fddd16e
pbeardshear <pbeardshear@berkeley.edu>
2012-04-04 21:35:07 -0700

    fixed scrollbar issue

--- app/assets/javascripts/halls.js
+++ app/assets/javascripts/halls.js
@@ -1,9 +1,29 @@
 $(document).delegate('[data-role="navbar"] a', 'click', function () {
-	$('.contentPage').hide();
-	$('#' + $(this).attr('data-href')).show();
+	//$('.contentPage').hide();
+	//$('#' + $(this).attr('data-href')).show();
+	$.mobile.changePage($(this).attr("href"), {
+	  transition: 'none',
+	  changeHash: false
+	});
+	
+	return false;
 });
 
-$(document).delegate('.intervalSelection', 'click', function (e) {
-  // Change the embedded script tag
-  $('#pulseGraph').attr('src', $('#pulseGraph').attr('src').replace(/interval=\w*/, 'interval='+this.innerHTML));
+$("div[data-role=page]").bind("pagebeforeshow", function () {
+    // prevents a jumping "fixed" navbar
+    $.mobile.silentScroll(0);
 });
+
+/*
+$(document).ready(function () {
+    $("div[data-role=page]").bind("pagebeforeshow", function () {
+      // prevents a jumping "fixed" navbar
+      $.mobile.silentScroll(0);
+  });
+});
+*/
+
+//$(document).delegate('.intervalSelection', 'click', function (e) {
+  // Change the embedded script tag
+  // $('#pulseGraph').attr('src', $('#pulseGraph').attr('src').replace(/interval=\w*/, 'interval='+this.innerHTML));
+//});
