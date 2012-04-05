67494337c0fcea35760a3f195d63ee409f17334a
pbeardshear <pbeardshear@berkeley.edu>
2012-04-04 19:07:30 -0700

    made some view changes

--- app/assets/javascripts/halls.js
+++ app/assets/javascripts/halls.js
@@ -2,3 +2,8 @@ $(document).delegate('[data-role="navbar"] a', 'click', function () {
 	$('.contentPage').hide();
 	$('#' + $(this).attr('data-href')).show();
 });
+
+$(document).delegate('.intervalSelection', 'click', function (e) {
+  // Change the embedded script tag
+  $('#pulseGraph').attr('src', $('#pulseGraph').attr('src').replace(/interval=\w*/, 'interval='+this.innerHTML));
+});
