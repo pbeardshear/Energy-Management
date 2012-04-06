4430e1de2993e0de2f6a14747f4c32ed2a7a0031
Huan Do <doboy0@gmail.com>
2012-04-06 14:19:43 -0700

    added basic tips admin stuff

--- app/controllers/admin/tips_controller.rb
+++ app/controllers/admin/tips_controller.rb
@@ -1,2 +1,23 @@
 class Admin::TipsController < ApplicationController
+  # TODO: add logic for failure, and add flash messages
+  
+  def index
+    @tips = Tip.all
+  end
+
+  def show
+    @tip = Tip.find(params[:id])
+  end
+
+  def delete
+    @tip = Tip.find(params[:id])
+    @tip.destory
+    redirect tips_index_page
+  end
+
+  def create
+    @tip.new(params).save
+    redirect tips_index_page
+  end
+
 end
