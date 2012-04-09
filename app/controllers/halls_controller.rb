4f5d08e6e5148ec4da12eeae0ebf156b20885110
pbeardshear <pbeardshear@berkeley.edu>
2012-04-08 20:45:58 -0700

    fixed interval bug

--- app/controllers/halls_controller.rb
+++ app/controllers/halls_controller.rb
@@ -4,8 +4,8 @@ class HallsController < ApplicationController
 
   def show
     @hall = Hall.find_by_id(Integer(params[:id]))
-    @interval = params[:interval]
-    @path = @hall.get_graph(300, 260, @interval || "week")
+    @interval = params[:interval] || "week"
+    @path = @hall.get_graph(300, 260, @interval)
     @green_features = @hall.green_features
   end
 end
