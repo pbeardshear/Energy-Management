fc9fbec58045ecc919c72dda0d3dd5bb64d94bf8
William Ho <redfedora404@gmail.com>
2012-04-13 15:23:52 -0700

    update category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -32,6 +32,29 @@ describe 'Category Spec' do
       @category.should have(1).tips(true)
       @tip.categories[0].name.should == "Cat1"
     end
+    
+    it 'can have more than 1 tip' do
+      @category = Category.new(:name=>'Cat1')
+      @category.tips.should be_empty
+      @tip1 = FactoryGirl.create(:tip, { :title => 'tip1', :content => 'Simply unplug items that you dont use very often.' })
+      @tip2 = FactoryGirl.create(:tip, { :title => 'tip2', :content => 'Simply unplug items that you dont use very often.' })
+      @tip_category1 = FactoryGirl.create(:category_tip, {:tip=>@tip1, :category=>@category})
+      @tip_category1.tip.title.should == 'tip1'
+      @tip_category2 = FactoryGirl.create(:category_tip, {:tip=>@tip2, :category=>@category})
+      @tip_category2.tip.title.should == 'tip2'
+      @tip1.should have(1).categories
+      @tip2.should have(1).categories
+      
+      @category.should have(2).tips(true)
+      @tip1.categories[0].name.should == "Cat1"
+      @tip2.categories[0].name.should == "Cat1"
+      
+      @category.tips[0].title.should == 'tip1'
+      @category.tips[1].title.should == 'tip2'
+    
+      
+    end
+    
   end
   
 end
