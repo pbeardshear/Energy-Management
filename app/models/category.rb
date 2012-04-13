4d3a2f06b3bf8fb82c1be4fe8c0c08c80a393bff
William Ho <redfedora404@gmail.com>
2012-04-13 14:02:19 -0700

    tips model specs

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
+  has_many :category_tips
   has_many :tips, :through => :category_tips
   has_many :settings, :through => :device_categories
 end
--- spec/controllers/admin/tips_controller_spec.rb
+++ spec/controllers/admin/tips_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Admin::TipsController do
 
   before :each do 
-    @tip1 = FactoryGirl.create(:tip, { :title => 'Unplug!', :content => 'Simply unplug items that you don’t use very often.' })
+    @tip1 = FactoryGirl.create(:tip, { :title => 'Unplug!', :content => 'Simply unplug items that you dont use very often.' })
     @tip2 = FactoryGirl.create(:tip, { :title => 'Turn Off BioSafety Cabinets', :content => 'Shut sashes and turn off blowers and lights at night.' })
     @category_general = FactoryGirl.create(:category, { :name => 'General' })
     @category_lab = FactoryGirl.create(:category, { :name => 'Lab' })
