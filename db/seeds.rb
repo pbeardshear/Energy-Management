a6da707cf1da337a72dc56aa3b9316d750010362
William Ho <redfedora404@gmail.com>
2012-04-13 12:30:15 -0700

    remove swap files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,10 @@ if Hall.where(:id => 1).empty?
   HallFeature.create(:hall_id => 1, :green_feature_id => 1)
 end
 
-# Adding categories  
-#Categories = {
-#  "General" => Category.create(:name => "General"),
-#  "Office" => Category.create(:name => "Office"),
-#  "Lab" => Category.create(:name => "Lab"),
-#  "Residence Halls" => Category.create(:name => "Residence Halls")
-#}
-
-# Adding tips
-#Tipz = {
-#  "Turn Off" => Tip.create(:title => "TURN OFF WHAT YOU'RE NOT USING", 
-#                           :content => "Monitors and lights when you’re away for more than 15 minutes"),
-#  "Buy" => Tip.create(:title => "Buy enegry efficient products",
-#                      :content => "Choose CFL or LED light bulbs instead of incandescents")
-#}
-
-# Add elements to the join table
-#CategoryTip.create( "category_id" => Categories["General"].id,
-                  #   "tips_id" => Tipz["Turn Off"].id )
-#CategoryTip.create( "category_id" => Categories["Office"].id,
-#                     "tips_id" => Tipz["Buy"].id )
-
 Category_general = Category.create(:name=>"General")
 Category_office = Category.create(:name=>"Office")
 Category_lab = Category.create(:name=>"Lab")
-Category_reshall = Category.create(:name=>"Residence Hall")
+Category_reshall = Category.create(:name=>"Residence Halls")
 
 generalTips = Array[
 Tip.create(:title=>"Turn off what you\'re not using", :content=>"Monitors and lights when you\'re away for more than 15 minutes"),
