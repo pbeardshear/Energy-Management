a90089c9fee7be9c12e6c0629591fc49a94f5645
pbeardshear <pbeardshear@berkeley.edu>
2012-04-20 21:53:23 -0700

    added view and controller for tips

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,6 +1,13 @@
 class TipsController < ApplicationController
   def index
-    @tips = Tip.all
+    @categories = {}
+    # Build up the possible categories
+    Tip.all.each do |tip|
+      tip.categories.each do |category|
+        @categories[category.name] = [] unless @categories[category.name]
+        @categories[category.name].push(tip)
+      end
+    end
   end
   
   def show
