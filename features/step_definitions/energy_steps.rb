d23b9a6c9f26c84ae4ace2e3cb78341762122ba5
Huan Do <doboy0@gmail.com>
2012-04-20 20:55:33 -0700

    replace slow, nondeterminstic selenium admin test with cucumber one

--- features/step_definitions/energy_steps.rb
+++ features/step_definitions/energy_steps.rb
@@ -1,61 +1,76 @@
-# -*- coding: utf-8 -*-
-Given /I am viewing "(.*)" hall/ do |hall|
-  pending
+Given /^I am not authenticated$/ do
+  visit('/admins/sign_out') # ensure that at least
 end
 
-When /I select "(.*)" from the time interval dropdown/ do |interval|
-  pending
+Given /^I am a new authenticated user$/ do
+  email = 'testing@man.net'
+  password = 'secretpass'
+  Admin.new(:email => email, :password => password, :password_confirmation => password).save!
+
+  visit '/admins/sign_in'
+  fill_in "admin_email", :with=>email
+  fill_in "admin_password", :with=>password
+  click_button "Sign In"
+
 end
 
-When /I select "(.*)" from the Operational Unit dropdown/ do |op|
-  pending
-end
+# Given /I am viewing "(.*)" hall/ do |hall|
+#   pending
+# end
 
-And /I search for "(.*)" hall/ do |hall|
-  pending
-end
+# When /I select "(.*)" from the time interval dropdown/ do |interval|
+#   pending
+# end
 
-Then /I should see "(.*)"* hall under the pinned tab/ do |hall|
-  pending
-end
+# When /I select "(.*)" from the Operational Unit dropdown/ do |op|
+#   pending
+# end
 
-Then /I should not see "(.*)"* hall under the pinned tab/ do |hall|
-  pending
-end
+# And /I search for "(.*)" hall/ do |hall|
+#   pending
+# end
 
-Given /the following halls are favorites: (.*)/ do |halls|
-  #halls.split(',')
-  pending
-end
+# Then /I should see "(.*)"* hall under the pinned tab/ do |hall|
+#   pending
+# end
 
-Then /^I should see the tip for "(.*)"$/ do |hall|
-  pending
-end
+# Then /I should not see "(.*)"* hall under the pinned tab/ do |hall|
+#   pending
+# end
 
-Then /^I should not see the tip for "(.*)"$/ do |hall|
-  pending
-end
+# Given /the following halls are favorites: (.*)/ do |halls|
+#   #halls.split(',')
+#   pending
+# end
 
-Given /^I work in an office$/ do
-  pending
-end
+# Then /^I should see the tip for "(.*)"$/ do |hall|
+#   pending
+# end
 
-Given /^I am in the residence halls$/ do
-  pending
-end
+# Then /^I should not see the tip for "(.*)"$/ do |hall|
+#   pending
+# end
 
-Then /^I should see "(.*)" hall$/ do |hall|
-  pending
-end
+# Given /^I work in an office$/ do
+#   pending
+# end
 
-Then /^I should not see "(.*)" hall$/ do |hall|
-  pending
-end
+# Given /^I am in the residence halls$/ do
+#   pending
+# end
 
-Then /^I should see no Halls$/ do
-  pending
-end
+# Then /^I should see "(.*)" hall$/ do |hall|
+#   pending
+# end
 
-Then /I should see the usage graph for the past week/ do
-  pending
-end
+# Then /^I should not see "(.*)" hall$/ do |hall|
+#   pending
+# end
+
+# Then /^I should see no Halls$/ do
+#   pending
+# end
+
+# Then /I should see the usage graph for the past week/ do
+#   pending
+# end
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,14 +13,8 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the dashboard view$/
-      '/'
-
-    when /^the overview page$/
-      '/'
-
-    when /^the energy tips page$/
-      '/'
+    when /^"(.*)"$/
+      $1
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
