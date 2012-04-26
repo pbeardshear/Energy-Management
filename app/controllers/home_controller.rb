faf76ef5f3e2a34dd61bbe29a91b447d82b9b2f3
pbeardshear <pbeardshear@berkeley.edu>
2012-04-25 17:09:23 -0700

    added more specs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,19 +3,19 @@ class HomeController < ApplicationController
   
   def index
     all_tips = Tip.all
-    general_tips = []
+    @general_tips = []
     # Compile the list of tips in the general category
     all_tips.each do |tip|
       tip.categories.each do |category|
         if category.name.downcase.include? "general"
-          general_tips.push(tip)
+          @general_tips.push(tip)
           break
         end
       end
     end
     # Convert the date into an integer
     time = Time.new
-    index = (time.month + time.day) % general_tips.length
-    @tip_of_the_day = general_tips[index]
+    index = (time.month + time.day) % @general_tips.length
+    @tip_of_the_day = @general_tips[index]
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe HomeController do
   before :each do
-    general_category = FactoryGirl.create(:category, { :name => 'General' })
-    @tip = FactoryGirl.create(:tip, { :title => 'This is a tip', :content => 'Turn off all the things!', :categories => [general_category] })
-    @other_tip = FactoryGirl.create(:tip, { :title => 'Problem solved!', :content => 'Drop an ice cube in the ocean every now and then', :categories => [general_category] })
-    Tip.stub(:all).and_return([@tip, @other_tip])
+    @general_category = FactoryGirl.create(:category, { :name => 'General' })
+    @lab_category = FactoryGirl.create(:category, { :name => 'Lab' })
+    @tip = FactoryGirl.create(:tip, { :title => 'This is a tip', :content => 'Turn off all the things!', :categories => [@general_category] })
+    @other_tip = FactoryGirl.create(:tip, { :title => 'Problem solved!', :content => 'Drop an ice cube in the ocean every now and then', :categories => [@general_category] })
+    @lab_tip = FactoryGirl.create(:tip, { :title => 'Boom!', :content => 'I know . . . we\'ll use science!', :categories => [@lab_category] })
+    Tip.stub(:all).and_return([@tip, @other_tip, @lab_tip])
   end
   describe 'displaying the tip for today' do
     it 'should display the correct tip for today' do
@@ -14,6 +16,22 @@ describe HomeController do
       Tip.should_receive(:all)
       get :index
       assigns(:tip_of_the_day).should == (index == 0 ? @tip : @other_tip)
+    end
+    
+    it 'should ignore any tips not in the general category' do
+      get :index
+      assigns(:general_tips).length.should == 2
+    end
+    
+    it 'should include tips which have more than one category' do
+      @multi_tip = FactoryGirl.create(:tip, { :title => 'Protip', :content => 'Your argument is invalid.', :categories => [@general_category, @lab_category] })      
+      Tip.stub(:all).and_return([@tip, @other_tip, @lab_tip, @multi_tip])
+      time = Time.new
+      index = (time.month + time.day) % 3;
+      get :index
+      assigns(:general_tips).length.should == 3
+      tips = [@tip, @other_tip, @multi_tip]
+      assigns(:tip_of_the_day).should == tips[index]
     end
   end
 end
